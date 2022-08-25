3b26332cbd6abc9c1e8d8f031799ed154f64ebea
T.Y <tomyamamt@gmail.com>
2022-08-25 11:32:17 +0900

    偶数と奇数が入れ替わっていた不具合を修正

--- components/CounterPinia.vue
+++ components/CounterPinia.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>{{ counter.count }}/{{ counter.isOdd?'偶数':"奇数" }}</p>
+    <p>{{ counter.count }}/{{ counter.isOdd?'奇数':"偶数" }}</p>
     <my-button id="inc-btn" text="inc" @click="inc" />
     <my-button id="dec-btn" text="dec" @click="dec" />
   </div>
